769a65bf77f1ca9388ca1e4527b1af2138badc26
sean <shonum@gmail.com>
2022-04-25 18:15:50 +0800

    c: make hardhat typescript

---
+++ scripts/run.ts
@@ -0,0 +1,20 @@
+import { ethers } from 'hardhat'
+
+const main = async () => {
+  const nftContractFactory = await ethers.getContractFactory('MyEpicNFT')
+  const nftContract = await nftContractFactory.deploy()
+  await nftContract.deployed()
+  console.log('Contract deployed to:', nftContract.address)
+}
+
+const runMain = async () => {
+  try {
+    await main()
+    process.exit(0)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+runMain()
