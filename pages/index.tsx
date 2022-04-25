155c904f12a84f8fbfbf2a570b2c644fe9aa0c9b
sean <shonum@gmail.com>
2022-04-25 10:35:41 +0800

    testing precommit again

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 export default function Landing() {
-  // testing husky
+  // testing husky again
   return (
     <div className="flex justify-center px-8 pt-8">
       <div className="w-full max-w-s">The landing page goes here.</div>
