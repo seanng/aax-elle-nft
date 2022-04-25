922890f1e1d28b7a2cb44ea0a15a408d7617d5e4
sean <shonum@gmail.com>
2022-04-25 10:32:52 +0800

    INITIAL COMMIT

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Landing() {
+  return (
+    <div className="flex justify-center px-8 pt-8">
+      <div className="w-full max-w-s">The landing page goes here.</div>
+    </div>
+  );
+}
