2938eb52c1be28bf7f257fb5a2a0b80a02bc7e95
sean <shonum@gmail.com>
2022-06-06 17:38:23 -0400

    remove unused files

--- __tests__/backend/services/encrypted-images.test.ts
+++
@@ -1,9 +0,0 @@
-import { prismaMock } from 'utils/mocks'
-// Order of imports is important
-import { getRandomEncryptedImage } from 'backend/services/encrypted-images'
-
-describe('services/encrypted-images', () => {
-  describe('getRandomEncryptedImage', () => {
-    test.skip('placeholder test', () => {})
-  })
-})
