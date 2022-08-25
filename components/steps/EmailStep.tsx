81f0324c6df15c44ef501afe8597e815c8b40cc8
sean <shonum@gmail.com>
2022-08-25 17:38:02 +0800

    s: style email and passcode step

--- components/steps/EmailStep.tsx
+++ components/steps/EmailStep.tsx
@@ -31,33 +31,37 @@ export function EmailStep({ updateForm, ...wizard }: Props) {
 
   return (
     <form onSubmit={handleSubmit(handleFormSubmit)}>
-      <div className="flex flex-col items-center font-noto pt-16">
-        <FormStamp className="mb-4">1</FormStamp>
-        <FormHeading className="mb-2">輸入你的 Email</FormHeading>
-        <div className="w-80 mb-10">
-          <p className="text-white mb-10 text-center">
-            噓...
-            為你的秘密加密上鎖，輸入他/她的email後，對方將會收到通知和你設定的專屬密碼，決定是否揭曉秘密。
+      <div className="flex flex-col items-center font-noto mt-12">
+        <FormStamp className="mb-4 md:mb-7">1</FormStamp>
+        <FormHeading className="mb-2 md:mb-6">輸入你的 Email</FormHeading>
+        <div className="w-full mb-10 flex flex-col items-center">
+          <p className="text-center text-white md:text-2xl leading-150% mb-10 md:mb-20">
+            鑄造完畢後你會收到專屬Love Message
+            <br /> 也會不定時空投賦能資訊喔
           </p>
-          <input
-            id="email"
-            type="text"
-            placeholder="Your email"
-            className="text-black border-lime w-full mb-2 font-mono placeholder-slate-500 bg-lime border-transparent focus:border-transparent focus:ring-0"
-            {...register('email', {
-              required: true,
-              pattern: {
-                value: EMAIL_REGEX,
-                message: '!',
-              },
-            })}
-          />
-          {errors?.email?.message && (
-            <div className="flex items-center">
-              <FormErrorIcon />
-              <span className="ml-2 text-sm">Email 格式不正確</span>
-            </div>
-          )}
+          <div className="w-80 md:w-96 md:mb-20">
+            <input
+              id="email"
+              type="text"
+              placeholder="Your email"
+              className="text-black md:text-2xl border-lime font-mono placeholder-slate-500 bg-lime border-transparent focus:border-transparent focus:ring-0 w-full mb-2"
+              {...register('email', {
+                required: true,
+                pattern: {
+                  value: EMAIL_REGEX,
+                  message: '!',
+                },
+              })}
+            />
+            {errors?.email?.message && (
+              <div className="flex items-center">
+                <FormErrorIcon />
+                <span className="ml-2 text-sm md:text-base">
+                  Email 格式不正確
+                </span>
+              </div>
+            )}
+          </div>
         </div>
         <div className="flex space-x-8">
           <SecondaryButton type="button" onClick={handleBackClick}>
--- components/steps/PasscodeStep.tsx
+++ components/steps/PasscodeStep.tsx
@@ -30,37 +30,40 @@ export function PasscodeStep({ updateForm, ...wizard }: Props) {
 
   return (
     <form onSubmit={handleSubmit(handleFormSubmit)}>
-      <div className="flex flex-col items-center font-noto pt-16">
-        <FormStamp className="mb-4">2</FormStamp>
-        <FormHeading className="mb-2">自訂專屬你們的密碼</FormHeading>
-        <div className="w-80 mb-10">
-          <p className="text-white mb-10 text-center">
-            噓...
-            為你的秘密加密上鎖，輸入他/她的email後，對方將會收到通知和你設定的專屬密碼，決定是否揭曉秘密。
+      <div className="flex flex-col items-center font-noto mt-12">
+        <FormStamp className="mb-4 md:mb-7">2</FormStamp>
+        <FormHeading className="mb-2 md:mb-6">自訂專屬你們的密碼</FormHeading>
+        <div className="w-full mb-10 flex flex-col items-center">
+          <p className="text-center text-white md:text-2xl leading-150% mb-10 md:mb-20">
+            噓… 先幫這份愛加密上鎖，分享給對方
+            <br />
+            後，他可以決定是否透過專屬密碼來解密！
           </p>
-          <input
-            id="passcode"
-            type="text"
-            placeholder="解鎖密碼"
-            className="text-black border-lime w-full mb-2 font-mono placeholder-slate-500 bg-lime border-transparent focus:border-transparent focus:ring-0"
-            {...register('passcode', {
-              required: true,
-              pattern: {
-                value: /^[a-z0-9]+$/i,
-                message: '!',
-              },
-            })}
-          />
-          {errors?.passcode?.message ? (
-            <div className="flex items-center">
-              <FormErrorIcon />
-              <span className="ml-2 text-sm">
-                格式錯誤，請輸入英文字母和字數
-              </span>
-            </div>
-          ) : (
-            <span className="text-sm">僅限英文字母和數字</span>
-          )}
+          <div className="w-80 md:w-96 md:mb-20">
+            <input
+              id="passcode"
+              type="text"
+              placeholder="解鎖密碼"
+              className="text-black md:text-2xl border-lime font-mono placeholder-slate-500 bg-lime border-transparent focus:border-transparent focus:ring-0 w-full mb-2"
+              {...register('passcode', {
+                required: true,
+                pattern: {
+                  value: /^[a-z0-9]+$/i,
+                  message: '!',
+                },
+              })}
+            />
+            {errors?.passcode?.message ? (
+              <div className="flex items-center">
+                <FormErrorIcon />
+                <span className="ml-2 text-sm md:text-base">
+                  格式錯誤，請輸入英文字母和字數
+                </span>
+              </div>
+            ) : (
+              <span className="text-sm md:text-base">僅限英文字母和數字</span>
+            )}
+          </div>
         </div>
         <div className="flex space-x-8">
           <SecondaryButton type="button" onClick={handleBackClick}>
