6c320dc90c02a8048a34bf73f9802250ab6699c0
sean <shonum@gmail.com>
2022-08-26 19:25:29 +0800

    s: style desktop sharingmodal

--- components/modals/SharingModal.tsx
+++ components/modals/SharingModal.tsx
@@ -77,30 +77,34 @@ export function SharingModal({ isOpen, closeModal, files }: Props) {
           <div className="flex items-end sm:items-center justify-center min-h-full p-4 sm:p-0">
             <TransitionChild>
               <Dialog.Panel className="relative transition-all flex flex-col items-center">
-                <div className="h-4 w-[300px] bg-white" />
-                <div className="w-[330px] bg-white pt-7 text-center">
+                <div className="h-4 w-mobile-modal-top md:w-desktop-modal-top bg-white" />
+                <div className="w-mobile-modal-body md:w-desktop-modal-body bg-white pt-7 flex flex-col items-center">
                   <ModalCloseButton
                     onClick={closeModal}
                     className="absolute right-5 -top-10"
                   />
-                  <h1 className="text-xl mb-4">分享告白</h1>
-                  <div className="flex px-9 mb-6">
-                    <WarningIcon width={36} height={36} />
-                    <p className="text-orange text-lg text-left pl-2">
-                      鑄造告白才能得到彩色版 Impact NFT 並參加抽獎喔！
-                    </p>
-                  </div>
+                  <div className="flex flex-col w-full max-w-[350px]">
+                    <h1 className="text-xl md:text-3xl mb-4 text-center">
+                      分享告白
+                    </h1>
+                    <div className="flex mb-6 px-3">
+                      <WarningIcon width={36} height={36} />
+                      <p className="text-orange text-lg text-left pl-2">
+                        鑄造告白才能得到彩色版 Impact NFT 並參加抽獎喔！
+                      </p>
+                    </div>
 
-                  <div
-                    dangerouslySetInnerHTML={{ __html: preview as string }}
-                  />
-                  <SocialButtons
-                    className="my-4"
-                    onDLClick={handleDLClick}
-                    onIGClick={handleIGClick}
-                  />
+                    <div
+                      dangerouslySetInnerHTML={{ __html: preview as string }}
+                    />
+                    <SocialButtons
+                      className="my-4"
+                      onDLClick={handleDLClick}
+                      onIGClick={handleIGClick}
+                    />
+                  </div>
                 </div>
-                <div className="h-4 w-[300px] bg-white" />
+                <div className="h-4 w-mobile-modal-top md:w-desktop-modal-top bg-white" />
               </Dialog.Panel>
             </TransitionChild>
           </div>
