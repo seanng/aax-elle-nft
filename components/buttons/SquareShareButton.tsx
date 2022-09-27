5c381d018c8b7fd1c10c4467ba14feac148c30fb
sean <shonum@gmail.com>
2022-09-27 16:37:17 +0800

    s: update squaresharebutton style

--- components/buttons/SquareShareButton.tsx
+++ components/buttons/SquareShareButton.tsx
@@ -17,12 +17,19 @@ const SquareShareSvg = ({ width, height, ...props }) => (
     {...props}
   >
     <path fill="#EDFA00" d="M0 0h36v36H0z" />
+    <path fill="#000" d="M21 11h3v3h-3zM18 9h3v3h-3zM18 12h3v3h-3z" />
+    <path fill="#000" d="M16 14h3v3h-3zM13 15h3v3h-3z" />
+    <path fill="#000" d="M11 17h3v3h-3zM9 20h3v3H9zM8 23h3v3H8z" />
+    <path fill="#000" d="M7 25h3v2H7zM10 23h3v3h-3zM23 13h3v3h-3z" />
+    <path fill="#000" d="M25 15h3v3h-3z" />
+    <path fill="#000" d="M27 17h3v3h-3z" />
+    <path fill="#000" d="M25 19h3v3h-3z" />
     <path
-      d="M27 17.875 18.75 10v4.5C10.855 14.5 9 20.161 9 25.75c2.279-2.918 4.294-4.5 9.75-4.5v4.5L27 17.875Z"
-      stroke="#000"
-      strokeWidth={2}
-      strokeLinejoin="round"
+      fill="#000"
+      d="M23 21h3v3h-3zM18 25h3v3h-3zM18 13h3v3h-3zM18 20h3v3h-3z"
     />
+    <path fill="#000" d="M18 22h3v3h-3zM13 22h3v3h-3z" />
+    <path fill="#000" d="M15 21h3v3h-3zM21 23h3v3h-3z" />
     <path
       fill="#55F263"
       d="M1 36h1v1H1zM2 36h1v2H2zM3 36h1v3H3zM4 36h1v4H4zM5 36h1v5H5zM6 36h36v6H6zM36 6h6v30h-6zM36 5h5v1h-5zM36 3h3v1h-3zM36 4h4v1h-4zM36 1h1v1h-1zM36 2h2v1h-2z"
