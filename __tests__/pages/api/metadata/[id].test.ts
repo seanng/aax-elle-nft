e05e034735ca855b90161686eb006bfd3e1cf275
sean <shonum@gmail.com>
2022-10-28 15:41:24 +0800

    f: access db to see if metadata is wlt or prize

--- __tests__/pages/api/metadata/[id].test.ts
+++ __tests__/pages/api/metadata/[id].test.ts
@@ -1,6 +1,21 @@
 import handler from 'pages/api/metadata/[id]'
 import { createMocks, RequestMethod } from 'node-mocks-http'
 
+jest.mock('backend/services/prize-tokens', () => {
+  const originalModule = jest.requireActual('backend/services/prize-tokens')
+  return {
+    __esModule: true,
+    ...originalModule,
+    findMany: jest.fn().mockResolvedValue([
+      {
+        id: 'abcd1234',
+        tokenId: 13,
+        isPrivateSale: true,
+      },
+    ]),
+  }
+})
+
 describe('api/metadata/[id]', () => {
   let httpMockedData = {
     method: 'GET' as RequestMethod,
@@ -9,16 +24,16 @@ describe('api/metadata/[id]', () => {
 
   describe('GET', () => {
     it('successfully returns a MessageToken JSON if ID is even number', async () => {
-      const ODD_NUMBER = '310'
-      httpMockedData.query.id = ODD_NUMBER
+      const EVEN_NUMBER = '310'
+      httpMockedData.query.id = EVEN_NUMBER
       const { req, res } = createMocks(httpMockedData)
 
       await handler(req, res)
 
       expect(res._getJSONData()).toEqual({
         name: 'Message Token',
-        image: `https://elle-nft-dev.s3.ap-southeast-1.amazonaws.com/public/${ODD_NUMBER}.png`,
-        animation_url: `https://elle-nft-dev.s3.ap-southeast-1.amazonaws.com/public/${ODD_NUMBER}.html`,
+        image: `https://elle-nft-dev.s3.ap-southeast-1.amazonaws.com/public/${EVEN_NUMBER}.png`,
+        animation_url: `https://elle-nft-dev.s3.ap-southeast-1.amazonaws.com/public/${EVEN_NUMBER}.html`,
       })
     })
 
